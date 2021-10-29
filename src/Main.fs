f088ff76286ab219de1b5af877625e0c2863912a
modotte <runecroon@gmail.com>
2021-10-29 14:43:45 +0800

    Remove feliz stuff for now

--- src/Main.fs
+++ src/Main.fs
@@ -1,11 +1,40 @@
 module Main
 
-open App
 open Fable.Core.JsInterop
 open Elmish
 open Elmish.React
 
 importSideEffects "./styles/global.scss"
+
+open Feliz
+open Elmish
+
+type TodoEntry = {
+    Id: int
+    Description: string
+    Completed: bool
+}
+
+type Model = {
+    Entries: TodoEntry array
+    NewEntry: string
+}
+
+type Message =
+| AddedEntry
+| RemovedEntry
+
+let init () = ({Entries = [||]; NewEntry = ""}, Cmd.none)
+
+let update message (model: Model, cmd: Cmd<'a>) =
+    match message with
+    | AddedEntry -> (model, Cmd.none)
+    | RemovedEntry -> (model, Cmd.none)
+
+let view model dispatch =
+    Html.div [
+        Html.h2 [ prop.text "Hello world" ]
+    ]
 
 Program.mkSimple init update view
 |> Program.withReactSynchronous "feliz-app"
