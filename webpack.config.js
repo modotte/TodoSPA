9c29adb11db6986b84ea460268e73eb54efc8e67
modotte <runecroon@gmail.com>
2021-10-30 15:16:06 +0800

    Add index view

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ var CONFIG = {
     // See https://github.com/jantimon/html-webpack-plugin
     indexHtmlTemplate: "./src/index.html",
     fsharpEntry: "./src/Main.fs.js",
-    outputDir: "./dist",
+    outputDir: "./docs",
     assetsDir: "./public",
     devServerPort: 8080,
     // When using webpack-dev-server, you may need to redirect some calls
